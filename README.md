09653bf896767ebf91b614a7893b3036f568221c
Ceralor <ceralor@gmail.com>
2018-04-17 22:43:59 -0500

    Now with 100% more modulizing and CLI args

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Just run `python get_dick.py`
+#How long does it take to find 'dick' in 128 bytes at a time of /dev/urandom?
+
+Just run `python get_dick.py` and find out.
+
+You can also supply a byte integer amount, like `python get_dick.py 32`, to search in smaller or larger byte sets.
+
+#Can I use get_dick elsewhere?
+
+Sure! Just `from get_dick import get_dick` and call. You'll be returned a tuple of `(buffer,total_time)`.
--- get_dick.py
+++ get_dick.py
@@ -8,9 +8,16 @@ def get_dick(bytes=128):
     while "dick" not in str(buffer):
       buffer = f.read(bytes)
     total_time = time() - start_time
-    print("It took %s seconds to find 'dick' in %s bytes of /dev/urandom." % (total_time,bytes))
-    print(str(buffer))
-    print("You're welcome.")
+  return (buffer,total_time)
 
 if __name__ == "__main__":
-  get_dick()
+  import sys
+  bytes = 128
+  try:
+    bytes = int(sys.argv[1])
+  except:
+    pass
+  buffer, total_time = get_dick(bytes)
+  print("It took %s seconds to find 'dick' in %s bytes of /dev/urandom." % (total_time,bytes))
+  print(str(buffer))
+  print("You're welcome.")
